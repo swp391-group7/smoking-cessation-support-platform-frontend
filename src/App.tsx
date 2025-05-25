14ce234f6c17a465eda5ac1647e3e57fa003f56e
Võ Hoàng Minh <minh7n3@gmail.com>
2025-05-26 01:06:33 +0700

    chỉnh sửa lại cấu trúc thư mục
    cấu trúc component với ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,7 @@ import { Button } from "@/components/ui/button"
 const App = () => {
 
   return (
-    <div className="flex h-screen items-center justify-center flex-col gap-4">
-      <h1>
-        Welcome to My App
-      </h1>
+    <div className="flex h-screen items-center justify-center">
       <Button className="bg-blue-500 text-white hover:bg-blue-600">
         Click Me
       </Button>
