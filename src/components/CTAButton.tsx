f5ce50d1e7c2a234f31792ad0b330046a8ee7c89
Võ Hoàng Minh <minh7n3@gmail.com>
2025-06-21 20:52:52 +0700

    add folder compenent/admin

--- src/components/CTAButton.tsx
+++ src/components/CTAButton.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+
 import { motion } from 'framer-motion';
 import { Button } from './ui/button'; // Import Button từ Shadcn UI
-import { useNavigate, NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 
 const CTAButton = () => {
